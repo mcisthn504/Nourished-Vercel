c06b4e0c42373c9808c33edc9962abc835afb5c8
s319903- Mauro Andres Cruz Ruiz <yous319903@studenti.polito.it>
2024-12-10 16:56:07 +0100

    fdfd

--- src/components/auth0-features.js
+++ src/components/auth0-features.js
@@ -4,9 +4,9 @@ import { Auth0Feature } from "./auth0-feature";
 export const Auth0Features = () => {
   const featuresList = [
     {
-      title: "Daily Challenge",
+      title: "Identity Providers",
       description:
-        "Are you ready for your Daily Challenge?",
+        "Auth0 supports social providers such as Google, Facebook, and Twitter, along with Enterprise providers such as Microsoft Office 365, Google Apps, and Azure. You can also use any OAuth 2.0 Authorization Server.",
       resourceUrl: "https://auth0.com/docs/connections",
       icon: "https://cdn.auth0.com/blog/hello-auth0/identity-providers-logo.svg",
     },
