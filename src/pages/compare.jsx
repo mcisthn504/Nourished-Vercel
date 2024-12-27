bbd1dd733fdf1251ee84f1c26119b96639373f2b
s319903- Mauro Andres Cruz Ruiz <s319903@studenti.polito.it>
2024-12-27 19:34:58 +0100

    Compare page front end done we need to add the rendering

--- src/pages/compare.jsx
+++ src/pages/compare.jsx
@@ -14,43 +14,45 @@ function Compare() {
         <h1 className="title">Compare</h1>
       </div>
       <div className="compare-container">
-        <div className="triangle-container left-triangle">
-          <div className="button-container">
-            <button
-              className="button"
-              onClick={() => (window.location.href = "/compare/camera")}
-            >
-              <i className="material-icons">camera_alt</i>
-              Camera
-            </button>
-            <button
-              className="button"
-              onClick={() => (window.location.href = "/compare/search")}
-            >
-              <i className="material-icons">search</i>
-              Search
-            </button>
-          </div>
-        </div>
-        <div className="triangle-container right-triangle">
-          <div className="button-container">
-            <button
-              className="button"
-              onClick={() => (window.location.href = "/compare/camera")}
-            >
-              <i className="material-icons">camera_alt</i>
-              Camera
-            </button>
-            <button
-              className="button"
-              onClick={() => (window.location.href = "/compare/search")}
-            >
-              <i className="material-icons">search</i>
-              Search
-            </button>
-          </div>
-        </div>
-      </div>
+  <div className="triangle-container left-triangle">
+    <div className="button-container-left">
+      <button
+        className="button"
+        onClick={() => (window.location.href = "/compare/camera")}
+      >
+        <i className="material-icons">camera_alt</i>
+        Camera
+      </button>
+      <button
+        className="button"
+        onClick={() => (window.location.href = "/compare/search")}
+      >
+        <i className="material-icons">search</i>
+        Search
+      </button>
+    </div>
+  </div>
+  <div className="triangle-container right-triangle">
+    <div className="button-container-right">
+      <button
+        className="button"
+        onClick={() => (window.location.href = "/compare/camera")}
+      >
+        <i className="material-icons">camera_alt</i>
+        Camera
+      </button>
+      <button
+        className="button"
+        onClick={() => (window.location.href = "/compare/search")}
+      >
+        <i className="material-icons">search</i>
+        Search
+      </button>
+    </div>
+  </div>
+  <div className="vs-circle">VS</div> {/* Add this element */}
+</div>
+
     </div>
   );
 }
